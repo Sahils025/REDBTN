caa28769fc27f07f85495455242f4550eec595cc
Sahils025 <ssahil25saini@gmail.com>
2026-06-08 10:43:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+
+    button.classList.add("pressed");
+
+    const timestamp = new Date().toLocaleString("en-IN", {
+        timeZone: "Asia/Kolkata",
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+
+    fetch("https://hook.eu1.make.com/ejjz1d7hrxl9m5ir2j2p7kcnzo6bj3r4", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            time: timestamp
+        })
+    });
+
+});
